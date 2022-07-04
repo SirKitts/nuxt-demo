c18e2d26cc3f1b097df7175e25790a14a081fb7b
sirkitts <sirkitts@gmail.com>
2022-07-04 20:12:03 +1000

    removed links

--- pages/index-bak.vue
+++ pages/index-bak.vue
@@ -2,7 +2,25 @@
   <div class="container">
     <div>
       <Logo />
-      <h1 class="title">What's up?</h1>
+      <h1 class="title">nuxt-demo</h1>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
     </div>
   </div>
 </template>
